0bf4014d7f12fdcabc7eebaf7088c202ca98bea9
getlanced <henrymc381@gmail.com>
2020-03-04 21:31:07 +0800

    Add files via upload

---
+++ IIS/App_Code/ListRoomWebService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+/// <summary>
+/// Summary description for ListRoomWebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class ListRoomWebService : System.Web.Services.WebService
+{
+
+    public ListRoomWebService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public List<Student> RoomList(string term, string year, string stTime, string edTime, string rmName, string cDate )
+    {
+        var arr = new List<Student>();
+
+        var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Test"].ConnectionString);
+        var cmd = new SqlCommand("ShowStudentListBasedOnRoom", conn);
+
+
+        cmd.Parameters.Add("@SchoolTermName", SqlDbType.Char, 2).Value = term;
+        cmd.Parameters.Add("@SchoolYearName", SqlDbType.Char, 9).Value = year;
+        cmd.Parameters.Add("@ClassStartTime", SqlDbType.Time).Value = stTime;
+        cmd.Parameters.Add("@ClassEndTime",SqlDbType.Time).Value = edTime;
+        cmd.Parameters.Add("@RName", SqlDbType.NVarChar,10 ).Value = rmName;
+        cmd.Parameters.Add("@ClassDate", SqlDbType.Date).Value = cDate;
+
+        conn.Open();
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.ExecuteNonQuery();
+        var dataAdapter = new SqlDataAdapter(cmd);
+        var dt = new DataTable();
+        dataAdapter.Fill(dt);
+        foreach (DataRow dr in dt.Rows)
+        {
+            var st = new Student();
+            st.studNum = dr["Student Number"].ToString();
+            st.fullName = dr["Full Name"].ToString();
+            st.attStat = dr["AttendanceStatus"].ToString();
+            st.subSec = dr["Subject/Section"].ToString();
+            arr.Add(st);
+            st = null;
+        }
+
+        conn.Close();
+        return arr;
+    }
+
+    public class Student
+    {
+        public string studNum { get; set; }
+        public string fullName { get; set; }
+        public string attStat { get; set; }
+        public string subSec { get; set; }
+    }
+}
---
+++ IIS/App_Code/StudentSearchWebService.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for StudentSearchWebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class StudentSearchWebService : System.Web.Services.WebService
+{
+
+    public StudentSearchWebService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+    [WebMethod]
+    public List<Student> StudentSearch(string fullName)
+    {
+        var arr = new List<Student>();
+
+        var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Test"].ConnectionString);
+        var cmd = new SqlCommand("ShowStudentListBasedOnName", conn);
+        cmd.Parameters.Add("@FullName", SqlDbType.NVarChar).Value = fullName;
+
+        conn.Open();
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.ExecuteNonQuery();
+        var dataAdapter = new SqlDataAdapter(cmd);
+        var dt = new DataTable();
+        dataAdapter.Fill(dt);
+        foreach (DataRow dr in dt.Rows)
+        {
+            var st = new Student();
+            st.fullName = dr["Full Name"].ToString();
+            st.studNum = dr["Student Number"].ToString();
+            st.Room = dr["Room"].ToString();
+            st.subSec = dr["Subject/Section"].ToString();
+            st.termYear = dr["Term/Year"].ToString();
+            st.date = dr["Date"].ToString();
+            st.attStat = dr["AttendanceStatus"].ToString();
+
+            arr.Add(st);
+            st = null;
+        }
+
+        conn.Close();
+        return arr;
+    }
+    [WebMethod]
+    public List<Student> StudentSearchNo(string studNo)
+    {
+        var arr = new List<Student>();
+
+        var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Test"].ConnectionString);
+        var cmd = new SqlCommand("ShowStudentListBasedOnStudentNumber", conn);
+        cmd.Parameters.Add("@StudNo", SqlDbType.NVarChar,10).Value = studNo;
+
+        conn.Open();
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.ExecuteNonQuery();
+        var dataAdapter = new SqlDataAdapter(cmd);
+        var dt = new DataTable();
+        dataAdapter.Fill(dt);
+        foreach (DataRow dr in dt.Rows)
+        {
+            var st = new Student();
+            st.fullName = dr["Full Name"].ToString();
+            st.studNum = dr["Student Number"].ToString();
+            st.Room = dr["Room"].ToString();
+            st.subSec = dr["Subject/Section"].ToString();
+            st.termYear = dr["Term/Year"].ToString();
+            st.date = dr["Date"].ToString();
+            st.attStat = dr["AttendanceStatus"].ToString();
+
+            arr.Add(st);
+            st = null;
+        }
+
+        conn.Close();
+        return arr;
+    }
+
+    public class Student
+    {
+        public string fullName { get; set; }
+        public string studNum { get; set; }
+        public string Room { get; set; }
+        public string subSec { get; set; }
+        public string termYear { get; set; }
+        public string date { get; set; }
+        public string attStat { get; set; }
+    }
+
+}
