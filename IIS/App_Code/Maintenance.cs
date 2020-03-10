c5c90b06a6955e1f0f5f7d2e5551dbaa7f905ff6
kylealelis <aleliskyleangelo@yahoo.com>
2020-03-10 15:40:24 +0800

    Maintenance Progress

---
+++ IIS/App_Code/Maintenance.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for Maintenance
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class Maintenance : System.Web.Services.WebService
+{
+
+    public Maintenance()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public List<string> GetSchoolYears()
+    {
+        var arr = new List<string>();
+
+        var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Test"].ConnectionString);
+        var cmd = new SqlCommand("GetSchoolYears", conn);
+        
+        conn.Open();
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.ExecuteNonQuery();
+        var dataAdapter = new SqlDataAdapter(cmd);
+        var dt = new DataTable();
+        dataAdapter.Fill(dt);
+        foreach (DataRow dr in dt.Rows)
+        {
+            arr.Add(dr["YearName"].ToString());
+        }
+
+        conn.Close();
+        return arr;
+    }
+
+    [WebMethod]
+    public List<string> GetSections()
+    {
+        var arr = new List<string>();
+
+        var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Test"].ConnectionString);
+        var cmd = new SqlCommand("GetSections", conn);
+
+        conn.Open();
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.ExecuteNonQuery();
+        var dataAdapter = new SqlDataAdapter(cmd);
+        var dt = new DataTable();
+        dataAdapter.Fill(dt);
+        foreach (DataRow dr in dt.Rows)
+        {
+            arr.Add(dr["SectionName"].ToString());
+        }
+
+        conn.Close();
+        return arr;
+    }
+
+    [WebMethod]
+    public List<string> GetRooms()
+    {
+        var arr = new List<string>();
+
+        var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Test"].ConnectionString);
+        var cmd = new SqlCommand("GetRooms", conn);
+
+        conn.Open();
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.ExecuteNonQuery();
+        var dataAdapter = new SqlDataAdapter(cmd);
+        var dt = new DataTable();
+        dataAdapter.Fill(dt);
+        foreach (DataRow dr in dt.Rows)
+        {
+            arr.Add(dr["RoomName"].ToString());
+        }
+
+        conn.Close();
+        return arr;
+    }
+
+    [WebMethod]
+    public List<string> GetSubjects()
+    {
+        var arr = new List<string>();
+
+        var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Test"].ConnectionString);
+        var cmd = new SqlCommand("GetSubjects", conn);
+
+        conn.Open();
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.ExecuteNonQuery();
+        var dataAdapter = new SqlDataAdapter(cmd);
+        var dt = new DataTable();
+        dataAdapter.Fill(dt);
+        foreach (DataRow dr in dt.Rows)
+        {
+            arr.Add(dr["SubjectName"].ToString());
+        }
+
+        conn.Close();
+        return arr;
+    }
+
+    [WebMethod]
+    public void InsertSchoolYear(string SY)
+    {
+        var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Test"].ConnectionString);
+        var cmd = new SqlCommand("InsertSchoolYear", conn);
+
+        conn.Open();
+        cmd.Parameters.Add("@Year", SqlDbType.NChar, 9).Value = SY;
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+
+    [WebMethod]
+    public void InsertRoom(string R)
+    {
+        var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Test"].ConnectionString);
+        var cmd = new SqlCommand("InsertRoom", conn);
+
+        conn.Open();
+        cmd.Parameters.Add("@Room", SqlDbType.NVarChar, 10).Value = R;
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+
+    [WebMethod]
+    public void InsertSection(string Sec)
+    {
+        var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Test"].ConnectionString);
+        var cmd = new SqlCommand("InsertSection", conn);
+
+        conn.Open();
+        cmd.Parameters.Add("@Section", SqlDbType.NVarChar, 5).Value = Sec;
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+
+    [WebMethod]
+    public void InsertSubject(string Sub)
+    {
+        var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Test"].ConnectionString);
+        var cmd = new SqlCommand("InsertSubject", conn);
+
+        conn.Open();
+        cmd.Parameters.Add("@Subject", SqlDbType.NVarChar, 15).Value = Sub;
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+}
---
+++ QuizAttendance/QuizAttendance/maintenance.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuizAttendance
+{
+    /// <summary>
+    /// Interaction logic for maintenance.xaml
+    /// </summary>
+    public partial class maintenance : Page
+    {
+        public int ControlInt = 0; // 1 = SY, 2 = Sec, 3 = R, 4 = Subj
+
+        public maintenance()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateWithSchoolYears()
+        {
+            Results.Items.Clear();
+
+            var a = new Maintenance.MaintenanceSoapClient();
+
+            using (a)
+            {
+                foreach (string b in a.GetSchoolYears())
+                {
+                    Results.Items.Add(b);
+                }
+            }
+        }
+
+        private void UpdateWithSections()
+        {
+            Results.Items.Clear();
+
+            var a = new Maintenance.MaintenanceSoapClient();
+
+            using (a)
+            {
+                foreach (string b in a.GetSections())
+                {
+                    Results.Items.Add(b);
+                }
+            }
+        }
+
+        private void UpdateWithRooms()
+        {
+            Results.Items.Clear();
+
+            var a = new Maintenance.MaintenanceSoapClient();
+
+            using (a)
+            {
+                foreach (string b in a.GetRooms())
+                {
+                    Results.Items.Add(b);
+                }
+            }
+        }
+
+        private void UpdateWithSubjects()
+        {
+            Results.Items.Clear();
+
+            var a = new Maintenance.MaintenanceSoapClient();
+
+            using (a)
+            {
+                foreach (string b in a.GetSubjects())
+                {
+                    Results.Items.Add(b);
+                }
+            }
+        }
+
+        private void SchoolYear_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateWithSchoolYears();
+            ControlInt = 1;
+        }
+
+        private void Section_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateWithSections();
+            ControlInt = 2;
+        }
+
+        private void Room_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateWithRooms();
+            ControlInt = 3;
+        }
+
+        private void Subject_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateWithSubjects();
+            ControlInt = 4;
+        }
+
+        private void AddEntry_Click(object sender, RoutedEventArgs e)
+        {
+            if (ControlInt == 0)
+            {
+                MessageBox.Show("Please choose a record to add your data to.", "WARNING", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                if (Results.Items.Contains(Entry.Text))
+                {
+                    MessageBox.Show("The item you are trying to add has already been added.", "WARNING", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    var a = new Maintenance.MaintenanceSoapClient();
+                    switch (ControlInt)
+                    {
+                        case 1:
+                            a.InsertSchoolYear(Entry.Text);
+                            UpdateWithSchoolYears();
+                            break;
+                        case 2:
+                            a.InsertSection(Entry.Text);
+                            UpdateWithSections();
+                            break;
+                        case 3:
+                            a.InsertRoom(Entry.Text);
+                            UpdateWithRooms();
+                            break;
+                        case 4:
+                            a.InsertSubject(Entry.Text);
+                            UpdateWithSubjects();
+                            break;
+                    }
+                    MessageBox.Show("Record has been added.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                    Entry.Text = "";
+                }
+            }
+        }
+    }
+}
